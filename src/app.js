e25c2d2b6e2e688aba25ec074a1f984344079cd4
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-10-02 21:15:22 +0600

    routes added with prefix with router-handler

--- src/app.js
+++ src/app.js
@@ -9,17 +9,16 @@ app.use(cors({
     credentials: true,
 }));
 
-app.use(express.json({
-    limit: "16kb"
-}));
-
-app.use(express.urlencoded({
-    extended: true,
-    limit: "16kb"
-}))
-
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public")) // to serve static files such as images, CSS files, and JavaScript files from the "public" directory.
-
 app.use(cookieParser());
+
+
+// routes
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+app.use("/api/v1/users", userRouter); // prefix: localhost:8000/api/v1/users
 
 export default app;
